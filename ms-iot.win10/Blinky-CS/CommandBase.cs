5e8d144ad513d45ce33646fe6b57dd14b9bc3577
AndriusK <akrisiun@gmail.com>
2015-11-08 09:35:15 +0200

    Blinky  Gpio LED #47 success

---
+++ ms-iot.win10/Blinky-CS/CommandBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Blinky
+{
+    public class CommandBase : ICommand
+    {
+        public Action Execute { get; set; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return Execute != null;
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            Execute();
+        }
+    }
+
+    public static class AppCommands
+    {
+        public static ICollection<ICommand> commands { get; private set; }
+        static AppCommands() { commands = new Collection<ICommand>(); }
+
+        public static ICommand Add(ICommand cmd)
+        {
+            commands.Add(cmd);
+            return cmd;
+        }
+
+        public static ICommand AddAction(Action execute)
+        {
+            var cmd = new CommandBase { Execute = execute };
+            Add(cmd);
+            return cmd;
+        }
+
+    }
+
+}
--- ms-iot.win10/Blinky-CS/MainPage.xaml.cs
+++ ms-iot.win10/Blinky-CS/MainPage.xaml.cs
@@ -4,23 +4,32 @@ using System;
 using Windows.Devices.Gpio;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Media;
+//using IoTCoreDefaultApp.Utils;
 
 namespace Blinky
 {
     public sealed partial class MainPage : Page
     {
-        private const int LED_PIN = 5;
+        // const int LED_PIN = 5;
+        public int LED_PIN = 47; // on-board LED on the Rpi2
         private GpioPin pin;
         private GpioPinValue pinValue;
         private DispatcherTimer timer;
+
         private SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
+        private SolidColorBrush greenBrush = new SolidColorBrush(Windows.UI.Colors.Green);
         private SolidColorBrush grayBrush = new SolidColorBrush(Windows.UI.Colors.LightGray);
 
         public MainPage()
         {
             InitializeComponent();
+            btnShutdown.Command = AppCommands.AddAction(() => App.Current.Exit());
+
+            //if (DeviceTypeInformation.Type == DeviceTypes.DB410)
+            //{
+            //    LED_PIN = 115; // on-board LED on the DB410c
+            //}
 
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromMilliseconds(500);
@@ -29,12 +38,13 @@ namespace Blinky
             if (pin != null)
             {
                 timer.Start();
-            }        
+            }
         }
 
         private void InitGPIO()
         {
             var gpio = GpioController.GetDefault();
+            GpioPinInfo.Text = "Led pin: " + LED_PIN.ToString();
 
             // Show an error if there is no GPIO controller
             if (gpio == null)
@@ -53,18 +63,13 @@ namespace Blinky
 
         }
 
-   
-
-
-
-
         private void Timer_Tick(object sender, object e)
         {
             if (pinValue == GpioPinValue.High)
             {
                 pinValue = GpioPinValue.Low;
                 pin.Write(pinValue);
-                LED.Fill = redBrush;
+                LED.Fill = greenBrush;
             }
             else
             {
@@ -73,7 +78,7 @@ namespace Blinky
                 LED.Fill = grayBrush;
             }
         }
-             
+
 
     }
 }
