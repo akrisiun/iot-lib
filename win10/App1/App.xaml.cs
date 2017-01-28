ef9610efab71a496354dfef1aa4c817f5285ac10
AndriusK <akrisiun@gmail.com>
2017-01-28 05:21:23 +0200

    recompile with VS 2017 (MSBuild 15)

--- win10/App1/App.xaml.cs
+++ win10/App1/App.xaml.cs
@@ -28,9 +28,10 @@ namespace App1
         /// </summary>
         public App()
         {
-            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
-                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
-                Microsoft.ApplicationInsights.WindowsCollectors.Session);
+            //Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
+            //    Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
+            //    Microsoft.ApplicationInsights.WindowsCollectors.Session);
+
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
--- win10/build.cmd
+++ win10/build.cmd
@@ -1,3 +1,6 @@
-"c:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /v:m
+
+"c:\Program Files (x86)\MSBuild\15.0\Bin\MSBuild.exe" /v:m
+
+@REM "c:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /v:m
 
 @PAUSE
